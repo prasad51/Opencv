977285ec5f40b2fccf0f144a1f1d8a872b374f9f
prasad51 <mattaprasadg@gmail.com>
2020-10-06 19:35:36 +0530

    code change

--- opencv_video.py
+++ opencv_video.py
@@ -1,11 +1,14 @@
 import cv2
 
 video = cv2.VideoCapture(0)
+
 while True:
-    vid,frm = video.read(0)
+    vid,frm = video.read()
+
     if vid==True:
         cv2.imshow('image',frm)
         if cv2.waitKey(1) & 0xff==ord('q'):
             break
+
 video.release()
 cv2.destroyAllWindows()
